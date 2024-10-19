5528638d4a3948fb5101b6e6f41d1e573faece9f
Jean claro dos Santos <jeanclarodev@gmail.com>
2024-10-19 16:12:35 -0300

    end2

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ require('./config/database');
 app.use('/users', usersRouter);
 app.use('/transactions', transactionsRouter);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
