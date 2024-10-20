148b3dd33aba45c5a76244c8cdcd291c843d103e
Jean claro dos Santos <jeanclarodev@gmail.com>
2024-10-19 21:48:45 -0300

    appjg

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 
 // Configuração do CORS
 app.use(cors({
-  origin: 'https://frontend-gasta-pouco-an17qrmxo-jeanclarocodes-projects.vercel.app/', 
+  origin: 'https://frontend-gasta-pouco.vercel.app', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-access-token'],
   credentials: true
